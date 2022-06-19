65d6a211bbcb9db8a0a68289a50aa69edd344791
Yangchen Pan <yangchen@Yangchens-MacBook-Pro.local>
2022-06-19 14:26:54 -0400

    small update

--- agents/hillclimbingutils.py
+++ agents/hillclimbingutils.py
@@ -42,6 +42,7 @@ def projected_noisy_natural_gradient(env_name, noise_scale, ga_learning_rate, de
     s_updated = projection(env_name, s_updated, low, up)
     return s_updated, norm_ng_deltas, np.linalg.norm(s_updated - s, ord=2), deltas, normalized_ng_deltas
 
+
 def get_mixedqueue_init(hctype, erbuff, scqueue):
     if scqueue[VHC].getSize() == 0 or hctype == VHC:
         init_s, _, _, _, _ = erbuff.sample_batch(1)
@@ -76,14 +77,6 @@ def get_hctype(algo_name, name2q):
     hctype = name2q[algo_name]
     return hctype
 
-def update_ga_learning_rate(agent_function, add_rate):
-    agent_function.avg_addrate = 0.9 * agent_function.avg_addrate + 0.1 * add_rate
-    if agent_function.avg_addrate < 0.1:
-        agent_function.ga_learning_rate *= 2.0
-    elif agent_function.avg_addrate > 0.3:
-        agent_function.ga_learning_rate /= 2.0
-    agent_function.ga_learning_rate = np.clip(agent_function.ga_learning_rate, 0.01, 1.0)
-
 ''' --------------------------hill climbing search control-------------------- '''
 
 '''
@@ -93,11 +86,14 @@ def update_ga_learning_rate(agent_function, add_rate):
     relu always gives zero. 
 '''
 
+
 def backup_termination(s):
     return False
 
+
 def sdist(s, ss):
     return np.linalg.norm(s - ss, ord=2) / np.sqrt(np.squeeze(s).shape[0])
+
 
 def ga_hc_search_control(erbuff, scqueue, low, up, thres, covmat, model, check_termination, agent_func, name2q, prob=None):
 
@@ -145,78 +141,5 @@ def ga_hc_search_control(erbuff, scqueue, low, up, thres, covmat, model, check_t
             restarttimes += 1
         if nloop > maxgaloops:
             break
-    if update_count % 10000 == 0:
-        print('num of count is -------------------------------------------- ', n_added, nloop, restarttimes,
-              ga_learning_rate, agent_func.avg_addrate)
-    ''' if it is continuous control task, use dynamic ga learning rate '''
-    #if agent_func.actionBound is not None:
-    #    update_ga_learning_rate(agent_func, ((n_added+1.)/(nloop+1.))*(restarttimes+1.))
     ''' the algo return number of added samples per step '''
     return n_added/nloop
-
-
-'''
-those code are NOT in use
-def enumerate_transition(self, s, model):
-    nextstates = []
-    nextrewards = []
-    nextgammas = []
-    for act in range(self.actionDim):
-        sp, r, gamma = model(np.squeeze(s), act)
-        nextstates.append(sp)
-        nextrewards.append(r)
-        nextgammas.append(gamma)
-    return nextstates, nextrewards, nextgammas
-
-def rollout_hc_fetch_states(self, s, model, hctype=None):
-    if len(s.shape) < 2:
-        s = s[None, :]
-    bestind = None
-    nextstates, nextrewards, nextgammas = self.enumerate_transition(s.copy(), model)
-
-    if 'Frequency' in hctype:
-        stacked_nextstates = np.vstack(nextstates)
-        gradnorms = self.sess.run(self.grad_norm, feed_dict={self.state_input: stacked_nextstates})
-        bestind = np.argmax(gradnorms)
-    elif 'HessNorm' in hctype:
-        listnorms = []
-        for state in nextstates:
-            nm = self.sess.run(self.hessian_norm, feed_dict={self.state_input: state})
-            listnorms.append(nm)
-        bestind = np.argmax(listnorms)
-    elif 'TD' in hctype:
-        targets = self.compute_current_q(np.vstack(nextstates), np.array(nextrewards), np.array(nextgammas))
-        savalues = self.sess.run(self.sa_value,
-                      feed_dict={self.state_input: np.vstack(nextstates), self.action_input: range(self.actionDim)})
-        abstds = np.abs(targets - savalues)
-        bestind = np.argmax(abstds)
-    elif 'MixedSum' in hctype:
-        listnorms = []
-        for state in nextstates:
-            mixsum = self.sess.run(self.combined_sum, feed_dict={self.state_input: state})
-            listnorms.append(mixsum)
-        bestind = np.argmax(listnorms)
-    elif 'Value' in hctype:
-        svalues = self.sess.run(self.max_q_value, feed_dict={self.state_input: np.vstack(nextstates)})
-        bestind = np.argmax(svalues)
-    sp = nextstates[bestind]
-    gamma = nextgammas[bestind]
-    return np.squeeze(sp), gamma
-
-def rollout_hc_search_control(self, erbuff, scqueue, low, up, thres, covmat, model):
-    # find search-control type
-    hctype = self.get_hctype()
-    n_added = 0
-    s, _, _, _, _ = erbuff.sample_batch(1)
-    s = s[0, :]
-    while n_added < self.num_sc_samples:
-        s, gamma = self.rollout_hc_fetch_states(s, model, hctype)
-        if gamma == 0.0:
-            s, _, _, _, _ = erbuff.sample_batch(1)
-            hctype = self.get_hctype()
-            s = s[0, :]
-        else:
-            scqueue.add(s.copy())
-            n_added += 1
-    return n_added
-'''
